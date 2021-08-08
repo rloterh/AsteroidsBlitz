48359806ddfd03948642c86f3a0fcdaa33972120
rloterh <rloterh@gmail.com>
2021-08-08 18:31:44 +0000

    add index page and app source directories

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: "./src/main.js",
+  entry: "./src/js/main.js",
   output: {
     path:path.resolve(__dirname, "dist"),
     filename: "app.js",
