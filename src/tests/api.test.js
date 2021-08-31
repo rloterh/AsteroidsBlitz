be1866f4c051a229c437354811ec35f4e3238f40
rloterh <rloterh@gmail.com>
2021-08-31 10:25:57 +0000

    Add negative test units for API requests

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -42,3 +42,19 @@ describe('setScore', () => {
     };
   });
 });
+
+test('returns result if non-empty object for POST action', () => {
+  fetch.mockResponseOnce(JSON.stringify({ result: 'Leaderboard score created correctly.' }));
+  const onResponse = jest.fn();
+  const onError = jest.fn();
+
+  return setScore()
+    .then(onResponse)
+    .catch(onError)
+    .finally(() => {
+      expect(onResponse).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+
+      expect(onResponse.mock.calls[0][0]).toEqual({ result: 'Leaderboard score created correctly.' });
+    });
+});
